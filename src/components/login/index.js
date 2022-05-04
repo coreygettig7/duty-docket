8510880f6a367ce0e4e6e2ca57337f0390f189bb
cola-mars <86696492+cola-mars@users.noreply.github.com>
2022-05-04 19:02:57 -0400

    cola/issue #2: create css formatting for login page

--- src/components/login/index.js
+++ src/components/login/index.js
@@ -1,31 +1,44 @@
-import React, { Component } from 'react';
-export default class Login extends Component {
-  render() {
-    return (
-      <form>
-        <h3>Sign In</h3>
-        <div className='entry-field'>
+import React from 'react';
+import 'cirrus-ui';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEnvelope, faLock } from '@fortawesome/free-solid-svg-icons';
+
+const envelopeIcon = <FontAwesomeIcon icon={faEnvelope} />
+const lockIcon = <FontAwesomeIcon icon={faLock} />
+
+function Login() {
+  return (
+    <section id="login-form" className='form-wrapper card u-flex'>
+      <form className='w-40p card_container form-card p-4'>
+        <h3 className='dark-text'>Sign In</h3>
+        <div className='input-control pb-2'>
           <label>Email address</label>
           <input 
             type='email'
-            className='form-control'
+            className='form-control input-contains-icon'
             placeholder='Enter email'
-          />  
+          />
+            <span class="icon fa fa-wrapper">{envelopeIcon}</span>
         </div>
-        <div className='entry-field'>
+        <div className='input-control pb-2'>
           <label>Password</label>
           <input  
             type='password'
-            className='form-control'
+            className='input-control input-contains-icon'
             placeholder='password'
           />
+            <span class="icon fa fa-wrapper">{lockIcon}</span>
         </div>
         <div className='d-grid'>
-          <button type='submit' className='btn btn-primary'>
+          <button 
+            type='submit' 
+            className='btn bg-link text-white'>
             Submit
           </button>
         </div>
       </form>
-    )
-  }
+    </section>
+  )
 }
+
+export default Login;
