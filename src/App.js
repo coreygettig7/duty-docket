fafe68d09f24698ce73eb1688a1e7eb5e77fc86e
cola-mars <86696492+cola-mars@users.noreply.github.com>
2022-05-04 19:30:18 -0400

    install react-helmet package

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 // import logo from './logo.svg';
+import React from 'react';
+import {Helmet} from 'react-helmet';
 import './App.css';
 import Welcome from './pages/Welcome';
 
 function App() {
   return (
-    <div className='bg-color'>
+    <div>
+      <Helmet>
+        <style>{'body { background-color: #5E60CE}'}</style>
+      </Helmet>
       <h1 className='logo-text'>Duty Docket</h1>
       <Welcome />
     </div>
