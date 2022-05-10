1c8e820fd3d77881db1c71daaedbf40c737c9c7b
cola-mars <86696492+cola-mars@users.noreply.github.com>
2022-05-09 20:24:07 -0400

    remove node modules
    Former-commit-id: 4ca00826118cda72ab8c6cd3cd0f244a86d6086c

--- client/src/pages/Welcome.js
+++ client/src/pages/Welcome.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Login from '../components/Login';
+import DailyDuties from '../components/DailyDuties';
 
 const Welcome = () => {
   return(
     <main>
-      <Login />
+     
+      <DailyDuties />
     </main>
   )
 };
