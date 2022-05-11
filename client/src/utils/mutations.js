7681174319948ab653ad266902297a4e37f1ddcb
cola-mars <86696492+cola-mars@users.noreply.github.com>
2022-05-11 13:21:03 -0400

    create login and signup form functionality
    Former-commit-id: 600a8132bb6c75083225c72843a38469af22b630

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,27 @@
+import { gql } from '@apollo/client';
+
+// query to login
+export const LOGIN_USER = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+// query to create an account
+export const ADD_USER = gql`
+  mutation addUser($username: String!, $password: String!, $email: String!) {
+    addUser(username: $username, password: $password, email: $email) {
+      user {
+        username
+        email
+      }
+    
+    }
+  }
+`;
