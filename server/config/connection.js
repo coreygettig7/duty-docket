7893e3179081cd87634f361e7cfe0a7cce440896
cola-mars <86696492+cola-mars@users.noreply.github.com>
2022-05-11 21:41:19 -0400

    remove node modules for production
    Former-commit-id: adfd59c7d056065bcc448f329b212c0188db0c82

--- server/config/connection.js
+++ server/config/connection.js
@@ -3,7 +3,5 @@ const mongoose = require('mongoose');
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/duty-docket', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false
 });
 module.exports = mongoose.connection;
