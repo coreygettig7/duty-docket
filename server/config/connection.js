a747c4a9f0dbf5e6fa18860bae83cbe2b6080da3
kdingman <kcdonlan@yahoo.com>
2022-05-21 18:17:15 -0400

    matched server names

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/duty-docket', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/project-3-test', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
