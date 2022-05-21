6dcb0947a134353dda2acf756b0ca259da387a0a
kdingman <kcdonlan@yahoo.com>
2022-05-21 16:42:43 -0400

    mongodb

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/duty-docket', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1/duty-docket', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
