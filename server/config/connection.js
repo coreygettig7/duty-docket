eb40c2628450b8b42c6950f0d2e61096d3eebf32
kdingman <kcdonlan@yahoo.com>
2022-05-21 18:42:47 -0400

    name changes in package files

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/project-3-test', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/dutydocket', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
