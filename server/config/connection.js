3212f4f8efadb989de6308aa60357159efc4aafd
kdingman <kcdonlan@yahoo.com>
2022-05-22 08:35:21 -0400

    added createIndex and useFindandModify to connection

--- server/config/connection.js
+++ server/config/connection.js
@@ -3,5 +3,7 @@ const mongoose = require('mongoose');
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/dutydocket', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false
 });
 module.exports = mongoose.connection;
