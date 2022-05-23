8b015f36837c164fc68576da8acc97503efa7bbe
cola-mars <86696492+cola-mars@users.noreply.github.com>
2022-05-22 22:06:12 -0400

    add redirect after form submission
    Former-commit-id: dbc2524275446d01754566f2f33d24c2ca4250ec
    Former-commit-id: 5f92034e76f05e1da65bcd9d4ab0591af53dfe59

--- client/src/components/Delegation/index.js
+++ client/src/components/Delegation/index.js
@@ -1,8 +1,7 @@
 import React, { useState } from 'react';
 import 'cirrus-ui';
-import { useMutation, useQuery } from '@apollo/client';
+import { useMutation } from '@apollo/client';
 import { ADD_DUTY } from '../../utils/mutations';
-import { QUERY_ME, QUERY_ME_DUTIES } from '../../utils/queries';
 
 
 const Delegation = () => {
@@ -11,29 +10,7 @@ const Delegation = () => {
     const [dueDate, setDate] = useState('');
     const [dutyDeposit, setDeposit] = useState('');
 
-    const [addDuty, { error }] = useMutation(ADD_DUTY)
-    //     , {
-    //     update(cache, { data: { addDuty } }) {
-        
-    //       try {
-    //         // update me array's cache
-    //         const { me } = cache.readQuery({ query: QUERY_ME });
-    //         cache.writeQuery({
-    //           query: QUERY_ME,
-    //           data: { me: { ...me, duties: [...me.duties, addDuty] } },
-    //         });
-    //       } catch (e) {
-    //         console.warn("First duty!")
-    //       }
-    
-    //       // update thought array's cache
-    //       const { duties } = cache.readQuery({ query: QUERY_ME_DUTIES });
-    //       cache.writeQuery({
-    //         query: QUERY_ME_DUTIES,
-    //         data: { duties: [addDuty, ...duties] },
-    //       });
-    //     }
-    //   });
+    const [addDuty] = useMutation(ADD_DUTY)
 
     const handleFormSubmit = async (event) => {
         event.preventDefault();
@@ -46,6 +23,7 @@ const Delegation = () => {
         catch (e) {
             console.error(e);
         }
+        window.location.assign("/dashboard");
     };
 
     const handleDateChange = event => {
@@ -99,7 +77,6 @@ const Delegation = () => {
                 id="dutyDeposit"
             />
             <button type="submit" className="btn">Submit</button>
-            {error && <div>Please complete the form</div>}
         </form>
         </div>
     )
